c4895e3743ae8971895815c12043cf5b79ec9ce3
Serhii Sapiannyk <serhii09@icloud.com>
2022-02-14 12:22:40 +0100

    add removeCard / finish 18.5

--- src/Redux/cardRedux.js
+++ src/Redux/cardRedux.js
@@ -4,7 +4,7 @@ import strContains from "../utils/strContains"
 const createActionName = actionName => `app/cards/${actionName}`
 const ADD_CARD = createActionName('ADD_CARD')
 const TOGGLE_CARD_FAVORITE = createActionName('TOGGLE_CARD_FAVORITE')
-// const REMOVE_CARD = createActionName('REMOVE_CARD')
+const REMOVE_CARD = createActionName('REMOVE_CARD')
 
 //selectors
 export const getFilteredCards = ({cards, searchString}, columnId) => cards.filter(card => card.columnId === columnId && strContains(card.title, searchString.searchString) )
@@ -16,7 +16,7 @@ export const addCard = payload => ({ type: ADD_CARD, payload })
 
 export const getToggleCardFavorite = payload => ({ type: TOGGLE_CARD_FAVORITE, payload })
 
-
+export const removeCard = payload => ({ type: REMOVE_CARD, payload})
 
 const cardsReducer = (statePart = [], action) => {
   switch (action.type){
@@ -25,6 +25,9 @@ const cardsReducer = (statePart = [], action) => {
 
     case TOGGLE_CARD_FAVORITE:
       return statePart.map(card =>  (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card)
+
+      case REMOVE_CARD:
+        return statePart.filter(card => (card.id !== action.payload))
 
       default:
         return statePart
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './Card.module.scss'
-import { getToggleCardFavorite } from '../../Redux/cardRedux';
+import { getToggleCardFavorite, removeCard } from '../../Redux/cardRedux';
 import clsx from 'clsx';
 
 const Card = props => {
@@ -13,6 +13,10 @@ const Card = props => {
     dispatch( getToggleCardFavorite ( cardId ))
   }
 
+  const clickingRemove = e => {
+    e.preventDefault();
+    dispatch ( removeCard (cardId ))
+  }
 
   const card = useSelector(state => state.cards.filter(card => card.id === cardId))
   // console.log(card);
@@ -23,6 +27,9 @@ const Card = props => {
     <li className={styles.card}>{props.title}
       <button className={styles.buttonFavorite} onClick={clickingFavorite}>
         <span className={clsx(styles.icon, {[styles.active] : isFavorite}) + ' fa fa-star-o'}></span>
+      </button>
+      <button className={styles.buttonRemove} onClick={clickingRemove}>
+        <span className={styles.icon + ' fa fa-trash'}></span>
       </button>
     </li>
   )
